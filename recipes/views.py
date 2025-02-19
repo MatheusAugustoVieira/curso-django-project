8c97f4033e16c4df73e1a02a2080c2c514c32baa
Matheus Augusto Vieira <mthsav@gmail.com>
2025-02-18 21:19:55 -0300

    version 1.5

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from utils.recipes.factory import *
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Matheus Augusto',
+        'recipes': [make_recipe() for _ in range(10)],
 })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Matheus Augusto',
+        'recipe': make_recipe,
 })
