51deb13fa0b83db423a60a7ccd7e25e7c2f3ab2c
Matheus Augusto Vieira <mthsav@gmail.com>
2025-02-26 21:08:47 -0300

    version 2.7

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from utils.recipes.factory import *
+from django.http import HttpResponse
+
 from recipes.models import Recipe
 
-# Create your views here.
+
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
@@ -12,8 +14,13 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    
+    if not recipes:
+        return HttpResponse(content='Not found', status=404)
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | ',
 })
 
 
