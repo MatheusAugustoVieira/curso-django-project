88d0abe4196eac45718f49afad1b223dd9904496
Matheus Augusto Vieira <mthsav@gmail.com>
2025-03-03 23:32:53 -0300

    version 3.7

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+
+''' Essa sera a parte onde os usuarios irao colocar nas urls do seu navegador
+para ter acesso a determinadas areas do site ou navegar pelo site mesmo,
+qualquer alteracao feita na url pode acarretar em erro nos seus teste, porem 
+as urls devem ser fixas para cada site'''
+
 app_name = 'recipes'
 
 urlpatterns = [
