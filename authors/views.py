73c31d9a4f5058d6bc69e683190d685c91e9d6b3
Matheus Augusto Vieira <mthsav@gmail.com>
2025-03-17 18:07:50 -0300

    version 4.1

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .forms import RegisteForm
 
 # Create your views here.
 def register_view(request):
-    return render(request, 'authors/pages/register_view.html')
+    if request.POST:
+        form = RegisteForm(request.POST)
+    else:
+        form = RegisteForm()
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form,
+    })
